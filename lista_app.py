c989cc676b3b88420d110b98a5d4b8e69c17544d
chris985 <christianlonardoni@gmail.com>
2025-05-09 13:41:43 -0300

    commit

---
+++ lista_app.py
@@ -0,0 +1,52 @@
+import flet as ft
+from flet import AppBar, Text, View
+from flet.core.colors import Colors
+
+
+def main(page: ft.Page):
+    # Configurações
+    page.title = "Exemplo de Rotas"
+    page.theme_mode = ft.ThemeMode.DARK  # ou ft.ThemeMode.DARK
+    page.window.width = 375
+    page.window.height = 667
+
+    # Funções
+    def gerencia_rotas(e):
+        page.views.clear()
+        page.views.append(
+            View(
+                "/",
+                [
+                    AppBar(title=Text("Home"), bgcolor=Colors.PRIMARY_CONTAINER),
+                ],
+            )
+        )
+        if page.route == "/segunda":
+            page.views.append(
+                View(
+                    "/segunda",
+                    [
+                        AppBar(title=Text("Segunda tela"), bgcolor=Colors.SECONDARY_CONTAINER),
+                    ],
+                )
+            )
+        page.update()
+
+    def voltar(e):
+        page.views.pop()
+        top_view = page.views[-1]
+        page.go(top_view.route)
+
+
+    # Componentes
+
+
+    # Eventos
+    page.on_route_change = gerencia_rotas
+    page.on_view_pop = voltar
+
+    page.go(page.route)
+
+# Comando que executa o aplicativo
+# Deve estar sempre colado na linha
+ft.app(main)
